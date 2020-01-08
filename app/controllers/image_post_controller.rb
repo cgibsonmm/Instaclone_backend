834a4a21ab46f20d8299d770221afd01dd762360
corey gibson <cgibsonmm@gmail.com>
2020-01-08 14:43:19 -0500

    testing  requests

--- app/controllers/image_post_controller.rb
+++ app/controllers/image_post_controller.rb
@@ -3,4 +3,10 @@ class ImagePostController < ApplicationController
     @posts = ImagePost.with_attached_image.order("created_at DESC")
     render json: @posts
   end
+
+  def new
+    puts params
+    # @image_post = ImagePost.new()
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'image_post/index'
+  resources :image_post
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
